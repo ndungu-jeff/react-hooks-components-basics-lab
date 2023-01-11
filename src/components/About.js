f93adb100bc8eb881135bb8cc3267fd9760d2fab
jeff ndungu <ndungujeff.ke@gmail.com>
2023-01-11 17:07:32 +0300

    Done

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+function About() {
+    return (
+        <div id="about">
+            <a href="#about">About</a>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+
+
+function NavBar() {
+    return (
+      <nav>
+        <a href="#home">I'm a link!</a>
+      </nav>
+    );
+  }
+
+
+  export default NavBar
